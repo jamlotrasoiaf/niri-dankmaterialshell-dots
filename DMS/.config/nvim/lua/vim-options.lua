17862816988b24bd35331f81c8f8347b41460f63
jamlotrasoiaf <89862130+jamlotrasoiaf@users.noreply.github.com>
2025-08-13 19:34:41 +0530

    Update vim-options.lua

--- DMS/.config/nvim/lua/vim-options.lua
+++ DMS/.config/nvim/lua/vim-options.lua
@@ -4,7 +4,7 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
 vim.g.mapleader = " "
-vim.o.guifont = "Iosevka Nerd Font Mono:h13"
+vim.o.guifont = "Hack Nerd Font Mono:h13"
 vim.keymap.set({ "n", "x" }, "<leader>y", [["+y]]) -- copy to system clipboard
 vim.keymap.set({ "n", "x" }, "<leader>p", [["+p]]) -- paste from system clipboard
 vim.keymap.set('t', '<C-space>', "<C-\\><C-n>",{silent = true})
